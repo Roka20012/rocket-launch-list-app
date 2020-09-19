c124286be4435ba9ca9dd1a792d253e59a6193e4
Rostyslav Broslavskyi <broslavsky.rostislaw@gmail.com>
2020-09-20 01:53:02 +0300

    [feature] Add response limit to 5 items per request

--- app/store/launchList/api.ts
+++ app/store/launchList/api.ts
@@ -3,7 +3,7 @@ import client from '~/app/utils/apiInterceptor';
 // TODO: add type response for getLaunchList
 export const getLaunchList = async (): Promise<any> => {
   try {
-    const response = await client.get('launch');
+    const response = await client.get('launch/?limit=5');
 
     return response.data;
   } catch (error) {
