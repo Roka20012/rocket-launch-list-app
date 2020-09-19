750e15b4b49b8f8a40e494a2865974681adf252b
Rostyslav Broslavskyi <broslavsky.rostislaw@gmail.com>
2020-09-20 01:44:24 +0300

    [feature] Add apiInterceptor

---
+++ app/utils/apiInterceptor.ts
@@ -0,0 +1,31 @@
+import axios, { AxiosResponse, AxiosError } from 'axios';
+import { BASE_URL } from '@env';
+
+const client = axios.create({
+  baseURL: BASE_URL,
+});
+
+const onSuccess = (response: AxiosResponse) => response;
+const onError = async (error: AxiosError) => {
+  if (error.message === 'Network Error') {
+    console.error('NETWORK ERROR');
+
+    return Promise.reject({
+      message: 'Network error!',
+      type: 'clientError',
+      errros: null,
+      status: 1,
+    });
+  }
+
+  const response = error?.response?.data ? error.response.data : null;
+
+  return Promise.reject({
+    ...response,
+  });
+};
+
+client.interceptors.response.use(onSuccess, onError);
+client.defaults.headers['Cache-Control'] = 'no-cache';
+
+export default client;
