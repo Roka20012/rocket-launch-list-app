d36f01dc6dd9cf26ca8b6d7b1311596eebdf5c8a
Rostyslav Broslavskyi <broslavsky.rostislaw@gmail.com>
2020-09-20 01:43:21 +0300

    Init project

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  extends: [
+    '@react-native-community',
+    'airbnb-typescript',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+    '@typescript-eslint',
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    // need better config
+    'import/no-extraneous-dependencies': 'off',
+    // TS compiler will take care of this
+    'default-case': 'off',
+
+    // opinions:
+    'max-lines-per-function': ['error', 90],
+    'max-lines': ['error', 300],
+    '@typescript-eslint/no-unused-vars': 'off', // can be handy
+    '@typescript-eslint/no-use-before-define': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react-native/no-inline-styles': 'off',
+    'consistent-return': 'off',
+    'import/extensions': 'off',
+    'import/prefer-default-export': 'off',
+  },
+};
---
+++ babel.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  presets: [
+    'module:metro-react-native-babel-preset',
+    'module:react-native-dotenv',
+  ],
+  plugins: [
+    [
+      'babel-plugin-root-import',
+      {
+        rootPathSuffix: './',
+        rootPathPrefix: '~/',
+      },
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+          blacklist: null,
+          whitelist: null,
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
+    ],
+  ],
+};
